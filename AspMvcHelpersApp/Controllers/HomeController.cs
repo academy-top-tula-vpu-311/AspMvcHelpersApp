75944f818673aa8ce054cbee7a6a9d81f59e8b20
MaxEfimov <efimov@mail.ru>
2025-08-10 14:35:28 +0300

    10-08-25

--- AspMvcHelpersApp/Controllers/HomeController.cs
+++ AspMvcHelpersApp/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using AspMvcHelpersApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace AspMvcHelpersApp.Controllers
 {
@@ -8,24 +9,34 @@ namespace AspMvcHelpersApp.Controllers
     {
         string[] cities = { "Moscow", "St Petersburg", "Novosibirsk" };
         string[] users = { "Kenny", "Jimmy", "Sammy", "Poppy" };
+        string[] languages = { "english", "russian", "chines", "deutchen" };
 
         public IActionResult Index()
         {
             return View(new { Cities = cities, Users = users });
         }
 
-        public IActionResult User()
+        public new IActionResult User()
         {
             User user = new User()
             {
-                Id = 1,
-                Name = "Lenny",
-                Age = 28,
+                Id = 12,
+                Name = "Bobby",
+                Age = 32,
                 Gender = Gender.Male,
                 Languages = new() { "Russian", "English" }
             };
 
-            return View(user);
+            ViewBag.Languages = new SelectList(languages);
+            //return View(user);
+            //return View("UserModel", user);
+            //return View("UserEdit", user);
+            return View("UserTag");
+        }
+
+        public IActionResult Second()
+        {
+            return View();
         }
 
         public IActionResult Privacy()
--- AspMvcHelpersApp/Program.cs
+++ AspMvcHelpersApp/Program.cs
@@ -5,13 +5,6 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -23,5 +16,10 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
+    name: "user",
+    pattern: "User",
+    defaults: new { controller = "Home", action = "User" });
 
 app.Run();
